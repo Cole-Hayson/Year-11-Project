ac8cfbc7fad09af0baa01a96918e1f2666e43d7c
haysonc <haysonc@stu.otc.school.nz>
2021-08-09 10:32:30 +1200

    added a function named users_day. Asks user how there day is and outputs based on the answer.

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ while True:
     if start.upper() != "S":
         print('That input is invalid')
         start = input('To start the program please type "S"')
+
+
+def users_day():
+    good_day = input("\nAre you having a good day today?")
+    while True:
+        if good_day.lower() == "yes":
+            print("\nThat's very good to hear!")
+            break
+        if good_day.lower() == "no":
+            print("\nOh no, that's not very good")
+            break
+        if good_day.lower() != "yes" or "no":
+            print('\nPlease answer with "yes" or "no"')
+            good_day = input("\nAre you having a good day today?")
+
+
+users_day()
