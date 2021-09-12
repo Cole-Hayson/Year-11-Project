bddadba90e5c6a7d1f3a7f06b0858e26fb2b6d0b
haysonc <haysonc@stu.otc.school.nz>
2021-09-13 10:20:28 +1200

    minor improvements - added code that ignores spaces when user types an input. - fixed bug where program asks a question again even if user enters the write  input.

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def users_day():
     good_day = input("\nAre you having a good day today?")
     while True:
         # program outputs an answer based on how they respond.
-        if good_day.lower() == "yes":
+        if good_day.lower().strip() == "yes":
             print("\nThat's very good to hear!")
             break
-        if good_day.lower() == "no":
+        if good_day.lower().strip() == "no":
             print("\nOh no, that's not very good")
             break
-        if good_day.lower() != "yes" or "no":
+        if good_day.lower().strip() != "yes" or "no":
             # asks the question again if user enters wrong input.
             print('\nPlease answer with "yes" or "no"')
             good_day = input("\nAre you having a good day today?")
@@ -29,31 +29,33 @@ def compliment():
     while True:
         # asking user if they would like a compliment
         better_day = input("\nWould you like a random compliment to make your day a little better?\n")
-        if better_day.lower() == "yes":
+        if better_day.lower().strip() == "yes":
             # if user answers yes program generates a random compliment
             print(random.choice(compliment_list))
             while True:
                 # asks user if they would like another compliment
-                more_compliments = input("\nWould you like another random compliment")
-                if more_compliments.lower() == "yes":
+                more_compliments = input("\nWould you like another random compliment\n")
+                if more_compliments.lower().strip() == "yes":
                     print(random.choice(compliment_list))
-                elif more_compliments.lower() == "no":
+                elif more_compliments.lower().strip() == "no":
                     # if user answers no ends program
                     print("\nOkay, Goodbye! :)")
                     print("\nEnding Program...")
                     break
                 else:  # more_compliments.lower() != "yes" or more_compliments.lower() != "no":
                     print('\nPlease answer with "yes" or "no"')
-                    more_compliments = input("\nWould you like another compliment")
+                    # more_compliments = input("\nWould you like another compliment")
             break
-        if better_day.lower() == "no":
+        if better_day.lower().strip() == "no":
             # if user answers no program ends
             print("\nOkay, that's alright, Have a good day!")
             print("\nEnding Program...")
             break
-        if better_day.lower() != "yes" or "no":
+        # if better_day.lower().strip() != "yes" or "no":
+        else:
             # tells user to input the correct input
             print('\nPlease answer "yes" or "no"')
+            # better_day = input("\nWould you like a compliment to make your day a little better?\n")
 
 
 # code
@@ -62,11 +64,11 @@ print("Welcome, to Random Compliment Generator!")
 # asks user to type "s" inorder to begin the program.
 start = input('To start the program please type "S"')
 while True:
-    if start.upper() == "S":
+    if start.upper().strip() == "S":
         user_name = input("\nHello! What is your name?")
         print(user_name + "! That's a nice name :)")
         break
-    else:
+    if start.upper().strip() != "S":
         # asks the question again if user enters wrong input.
         print('That input is invalid')
         start = input('To start the program please type "S"')
